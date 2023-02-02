ae7468a28399b5fbf7456eba45bea1657f80ed71
Leonardo Loayza <leonardoloayza28@gmail.com>
2023-02-02 17:53:17 -0500

    gestor-credenciales-modal

--- src/components/Cards/card.tsx
+++ src/components/Cards/card.tsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 function Card() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
+            
         <div className="card">
             <div className="card-horizontal">
                 <div className="img-square-wrapper">
@@ -18,11 +26,33 @@ function Card() {
                     <div className="action_btn">
                         <button name="submit" className="action_btn submit" type="submit" value="Save" >Change password</button>
                         <button name="submit" className="action_btn cancel" >Enviar password</button>
+                        <Button variant="primary" onClick={handleShow}>
+                            Credenciales
+                        </Button>
+
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                            <Modal.Title>Credenciales</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>Por donde quieres recibir tus credenciales</Modal.Body>
+                            <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                SMS
+                            </Button>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Correo
+                            </Button>
+                            <Button variant="primary" onClick={handleClose}>
+                                Salir
+                            </Button>
+                            </Modal.Footer>
+                        </Modal>
                     </div>
                 </div>
                 <small className="text-muted">Last updated: fecha de actualizacion</small>
             </div>
         </div>
+        
     );
 };
 export default Card;
