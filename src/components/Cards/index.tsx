ffff966d057119fc909bb1d5ca2aab4754fe0f9c
Anthony Moises Condori Choccata <113246456+Alt-Anthony@users.noreply.github.com>
2023-02-04 11:36:47 -0500

    add offcanvas for user and delete profileuser carpet

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -2,20 +2,13 @@ import CardFunction from "./card"
 import "./Card.css"
 import { Row } from "react-bootstrap";
 function Cards() {
-    return(
-    <Row xs={1} md={2} className="g-4">
-                <div className="col-md-4">
-                    <CardFunction/>
-                </div>
-                <div className="col-md-4">
-                    <CardFunction/>
-                </div>
-                <div className="col-md-4">
-                    <CardFunction/>
-                </div>
-                <div className="col-md-4">
-                    <CardFunction/>
-                </div>
+    return (
+        <Row xs={1} md={2} className="g-4">
+            {Array.from({ length: 3 }).map((_, idx) => (
+            <div className="col-md-4">
+                <CardFunction />
+            </div>
+             ))}
         </Row>
     );
 };
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,29 +1,56 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getCurrentUser, logout } from "../../services/auth";
-import "./Navbar.css"
-import { faSignOut, faUserCheck, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { faEnvelope, faIdCard, faMailForward, faPhone, faSignOut, faUserCheck, faUserCircle, faUserEdit } from "@fortawesome/free-solid-svg-icons";
 import Dropdown from 'react-bootstrap/Dropdown';
 import "./Navbar.css";
+import { useState } from "react";
+
+
 
 function Navbar() {
+    //agregado anthony
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     //const obj = JSON.parse(atob(getCurrentUser().split('.')[1]));
     function handleLogout() {
         logout();
         window.location.reload();
+
     }
+
+
     return (
         <Dropdown>
             <div className="navbar bg-dark text-center text-white">
                 <div className="nav_logo"> <p className="d-inline mx-2">NombreUSUARIO</p></div>
                 <div className="nav_items">
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    <Dropdown.Toggle className="toggle" variant="success" id="dropdown-basic">
                         <FontAwesomeIcon icon={faUserCircle} /> Gestor
                     </Dropdown.Toggle>
                 </div>
             </div>
             <Dropdown.Menu>
-                <Dropdown.Item href="<ProfileUser/>"><FontAwesomeIcon icon={faUserCheck} /> Perfil de Usuario</Dropdown.Item>
-                <Dropdown.Item href="<LoginPage/>"><FontAwesomeIcon icon={faSignOut} />Cerrar Sesion</Dropdown.Item>
+                <Dropdown.Item onClick={handleShow}><FontAwesomeIcon icon={faUserCheck} /> Perfil de Usuario</Dropdown.Item>
+                <Offcanvas backdrop={false} placement={'end'} show={show} onHide={handleClose}>
+
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title><FontAwesomeIcon icon={faUserEdit} /> Perfil</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <h4><FontAwesomeIcon icon={faIdCard} /> Nombre y Apellidos</h4>
+                        <h5>user.name</h5>
+                        <br />
+                        <h4><FontAwesomeIcon icon={faPhone} /> Telefono</h4>
+                        <h5>user.phone_number</h5>
+                        <br />
+                        <h4><FontAwesomeIcon icon={faEnvelope} /> Email de Usuario</h4>
+                        <h5>user.email</h5>
+
+                    </Offcanvas.Body>
+                </Offcanvas>
+                <Dropdown.Item href="login"><FontAwesomeIcon icon={faSignOut} />Cerrar Sesion</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     );
